81226b18051680b4c8f82a72f06c57924e9db902
rodrigues_john <rodrigues_john@sicredi.com.br>
2024-06-27 21:22:28 -0300

    created model for db

---
+++ Migrations/20240628002000_createdDataBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class createdDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "TEXT", nullable: false),
+                    email = table.Column<string>(type: "TEXT", nullable: false),
+                    password = table.Column<string>(type: "TEXT", nullable: false),
+                    date_create = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    date_upgrade = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "TEXT", nullable: false),
+                    title = table.Column<string>(type: "TEXT", nullable: false),
+                    description = table.Column<string>(type: "TEXT", nullable: false),
+                    date_create = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    date_upgrade = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    user_id = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "Users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_user_id",
+                table: "Tasks",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ src/entities/TaskEntity.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace entities;
+
+public class TaskEntity {
+
+  [Key]
+  [Column("id")]
+  public string Id { get; init;}= Guid.NewGuid().ToString();
+      [Column("title")]
+      [Required]
+      public string? Title { get; set;}
+      [Column("description")]
+      [Required]
+      public string? Description { get; set;}
+      [Column("date_create")]
+      public DateTime DateCreate { get; set;}
+      [Column("date_upgrade")]
+      public DateTime DateUpgrade {get;set;}
+      [ForeignKey("user_id")]
+      [Column("user_id")]
+      public string? UserId {get;init;}
+
+      public UserEntity? User {get;set;}
+}
